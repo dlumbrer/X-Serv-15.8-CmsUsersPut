d910050879b2dc1656e0e215a565fd599d1866e3
David Moreno Lumbreras <dmorenolumb@gmail.com>
2015-04-06 14:16:47 +0200

    Terminado

--- myproject/cms_users_put/views.py
+++ myproject/cms_users_put/views.py
@@ -5,15 +5,35 @@ from models import Pages
 # Create your views here.
 
 def mostrar(request, recurso):
-    if request.method == "GET":
-        #MUESTRO DE LA BD
-        try:
-            fila = Pages.objects.get(name=recurso)
-            return HttpResponse(request.method + " " + str(fila.id) + " " + fila.name + " " + fila.page)
-        except Pages.DoesNotExist:
-            return HttpResponseNotFound("Page not found: " + recurso)
-    elif request.method == "PUT":
-        #GUARDO EN LA BD
-        p = Pages(name=recurso, page=request.body)
-        p.save()
-        return HttpResponse("guardada pagina, haz un get para comprobar")
+    #SOLO PUEDES HACER POST SI ESTAS AUTENTICADO
+    if request.user.is_authenticated():
+        salida = "Eres " + request.user.username + " " + "<a href='/logout'>logout</a>.  Puedes cambiar la page mediante un PUT<br><br>"
+        if request.method == "PUT":
+            p = Pages(name=recurso, page=request.body)
+            p.save()
+            return HttpResponse("guardada pagina, haz un get para comprobar")        
+    else:
+        salida = "No estas logueado <a href='/admin/login/'>Login</a>. No puedes cambiar la page<br><br>"
+    
+
+    try:
+        fila = Pages.objects.get(name=recurso)
+        salida += request.method + " " + str(fila.id) + " " + fila.name + " " + fila.page
+        return HttpResponse(salida)
+    except Pages.DoesNotExist:
+        salida += "Page not found: " + recurso
+        return HttpResponseNotFound(salida)
+
+
+def todo(request):
+    if request.user.is_authenticated():
+        salida = "Eres " + request.user.username + " " + "<a href='/logout'>logout</a><br><br>"
+    else:
+        salida = "No estas logueado <a href='/admin/login/'>Login</a><br><br>"
+            
+    lista = Pages.objects.all()
+    salida += "Esto es lo que tenemos:<ul>"
+    for fila in lista:
+        salida += "<li>" + fila.name + "->" + str(fila.page) + " "
+    salida += "</ul>"
+    return HttpResponse(salida)
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -6,6 +6,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'myproject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^logout$', "django.contrib.auth.views.logout"),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', "cms_users_put.views.todo"),
     url(r'^(.*)$', "cms_users_put.views.mostrar"),
 )
